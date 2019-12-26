65db3014b43723f98e9b2d8b32795d88d9f7e155
Marcin Celej <marcin.celej@gmail.com>
2019-12-26 17:17:43 +0100

    I added test generating swagger (open API) description file and it fails when it is changed. It makes sure that developer commits the changes to the API with the code. It also prevents from unintended API changes.

---
+++ Samples/Sample.Web.API/Web.API.Tests/SwaggerClient.cs
@@ -0,0 +1,54 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using NUnit.Framework;
+
+namespace Synergy.Samples.Web.API.Tests 
+{
+    public class SwaggerClient 
+    {
+        private readonly TestServer testServer;
+        private HttpClient httpClient => testServer.HttpClient;
+
+        public SwaggerClient(TestServer testServer)
+        {
+            this.testServer = testServer;
+        }
+
+        public async Task validate_api_contract_changes(string pathToContract, string version = "v1")
+        {
+            // ACT
+            var newContract = await GetCurrentSwaggerContract(version);
+
+            // ASSERT
+            var patternContract = ReadSwaggerContractFromSavedPattern(pathToContract);
+            var contracts = new JsonComparer(patternContract, newContract, "info.description");
+
+            if (contracts.AreEquivalent == false)
+            {
+                File.WriteAllText(pathToContract, newContract.ToString(Formatting.Indented));
+            }
+
+            Assert.IsTrue(contracts.AreEquivalent, $"Swagger API description was modified. Verify the differences:\n\n {contracts.GetDifferences()}");
+        }
+
+        private async Task<JObject> GetCurrentSwaggerContract([NotNull] string version)
+        {
+            var url = testServer.PrepareRequestUri($"/swagger/{version}/swagger.json");
+            var response = httpClient.GetAsync(url);
+            var contract = await response.Result.Content.ReadAsStringAsync();
+            return JObject.Parse(contract);
+        }
+
+        private static JObject ReadSwaggerContractFromSavedPattern(string pathToContract)
+        {
+            if (File.Exists(pathToContract) == false)
+                return JObject.Parse("{}");
+
+            return JObject.Parse(File.ReadAllText(pathToContract));
+        }
+    }
+}
---
+++ Samples/Sample.Web.API/Web.API.Tests/SwaggerTests.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Synergy.Samples.Web.API.Tests
+{
+    [TestFixture]
+    public class SwaggerTests
+    {
+        private const string Path = @"../../../Contracts/";
+
+        [Test]
+        [TestCase("v1")]
+        public async Task validate_api_contract_changes(string version)
+        {
+            // ARRANGE
+            var testServer = new TestServer();
+            var swagger = new SwaggerClient(testServer);
+
+            // ACT
+            await swagger.validate_api_contract_changes(Path + $"swagger-{version}.json", version);
+        }
+    }
+}
