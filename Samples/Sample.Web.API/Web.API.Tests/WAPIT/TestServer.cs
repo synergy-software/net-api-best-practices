692099659e3208695f0e801b936d806956e1edf4
Marcin Celej <marcin.celej@gmail.com>
2019-12-26 17:12:10 +0100

    I added classes to execute Web API tests (WAPITs)

---
+++ Samples/Sample.Web.API/Web.API.Tests/WAPIT/TestServer.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Net.Http;
+using JetBrains.Annotations;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Sample.Web;
+using Synergy.Contracts;
+using Synergy.Samples.Web.API.Extensions;
+
+namespace Synergy.Samples.Web.API.Tests
+{
+    public class TestServer
+    {
+        public HttpClient HttpClient { get; }
+
+        public TestServer()
+        {
+            var applicationFactory = StartupTestServerUsing<Startup>();
+            HttpClient = applicationFactory.CreateClient();
+        }
+
+        private WebApplicationFactory<TStartup> StartupTestServerUsing<TStartup>() where TStartup : class
+        {
+            return new WebApplicationFactory<TStartup>()
+                .WithWebHostBuilder(configuration =>
+                {
+                    configuration.UseEnvironment(ApplicationEnvironment.Tests);
+                });
+        }
+
+        public Uri PrepareRequestUri(string path, [CanBeNull] object? parameterToGet = null)
+        {
+            Fail.IfWhitespace(path, nameof(path));
+
+            var uriBuilder = new UriBuilder
+            {
+                Path = path,
+                Host = HttpClient.BaseAddress.Host,
+                Port = HttpClient.BaseAddress.Port
+            };
+
+            if (parameterToGet != null)
+                uriBuilder.Query = QueryBuilder.Build(parameterToGet);
+
+            return uriBuilder.Uri;
+        }
+    }
+}
