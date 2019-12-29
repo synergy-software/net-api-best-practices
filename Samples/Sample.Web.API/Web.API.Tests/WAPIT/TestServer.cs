cc9ac695367966e341fadddbbd65daf07ff878c4
Marcin Celej <marcin.celej@gmail.com>
2019-12-29 15:06:59 +0100

    I added method for testing POST operations in TestServer.

--- Samples/Sample.Web.API/Web.API.Tests/WAPIT/TestServer.cs
+++ Samples/Sample.Web.API/Web.API.Tests/WAPIT/TestServer.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Diagnostics;
 using System.Net.Http;
+using System.Text;
 using JetBrains.Annotations;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Newtonsoft.Json;
 using Sample.Web;
 using Synergy.Contracts;
 using Synergy.Samples.Web.API.Extensions;
@@ -13,7 +15,7 @@ namespace Synergy.Samples.Web.API.Tests
 {
     public class TestServer
     {
-        public HttpClient HttpClient { get; }
+        private HttpClient HttpClient { get; }
         public bool Repair { get; set; }
 
         public TestServer()
@@ -50,12 +52,33 @@ namespace Synergy.Samples.Web.API.Tests
 
         public HttpOperation Get(string path, [CanBeNull] object? queryParameters = null)
         {
-            var request = new HttpRequestMessage
-            {
-                Method = HttpMethod.Get,
-                RequestUri = this.PrepareRequestUri(path,  queryParameters)
-            };
+            var request = CreateHttpRequest(HttpMethod.Get, path, queryParameters);
+            return Send(request);
+        }
 
+        public HttpOperation Post(string path, [CanBeNull] object? urlParameters = null, object? content = null)
+        {
+            var request = CreateHttpRequest(HttpMethod.Post, path, urlParameters, content);
+            return Send(request);
+        }
+
+        private HttpRequestMessage CreateHttpRequest(HttpMethod httpMethod, string path, object? queryParameters, object? content = null)
+        {
+            var request = new HttpRequestMessage
+                                     {
+                                         Method = httpMethod,
+                                         RequestUri = this.PrepareRequestUri(path, queryParameters)
+                                     };
+            if (content != null)
+            {
+                request.Content = new StringContent(JsonConvert.SerializeObject(content), Encoding.UTF8, "application/json");
+            }
+
+            return request;
+        }
+
+        private HttpOperation Send(HttpRequestMessage request)
+        {
             Stopwatch timer = Stopwatch.StartNew();
             var response = this.HttpClient.SendAsync(request).Result;
             timer.Stop();
