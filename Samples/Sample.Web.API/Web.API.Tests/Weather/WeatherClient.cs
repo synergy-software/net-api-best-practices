0fcebab5b46d76d72be6ba3bcbe064f1e3194bc5
Marcin Celej <marcin.celej@gmail.com>
2019-12-29 10:48:42 +0100

    I added first Web API test (WAPIT) for GETting Wheather Forecast

---
+++ Samples/Sample.Web.API/Web.API.Tests/Weather/WeatherClient.cs
@@ -0,0 +1,16 @@
+﻿using Synergy.Samples.Web.API.Tests.WAPIT;
+
+namespace Synergy.Samples.Web.API.Tests.Weather
+{
+    public class WeatherClient
+    {
+        private readonly TestServer _testServer;
+
+        public WeatherClient(TestServer testServer)
+        {
+            _testServer = testServer;
+        }
+
+        public HttpOperation GetWeatherForecast() => _testServer.Get("api/v1/weather/forecast");
+    }
+}
---
+++ Samples/Sample.Web.API/Web.API.Tests/Weather/WeatherTests.cs
@@ -0,0 +1,36 @@
+﻿using System.Net;
+using JetBrains.Annotations;
+using NUnit.Framework;
+using Synergy.Samples.Web.API.Tests.WAPIT;
+
+namespace Synergy.Samples.Web.API.Tests.Weather
+{
+    [TestFixture]
+    public class WeatherTests
+    {
+        private const string Path = @"../../../Weather";
+
+        [Test]
+        public void get_weather()
+        {
+            // ARRANGE
+            var testServer = new TestServer();
+            var weather = new WeatherClient(testServer);
+
+            testServer.Repair = false;
+
+            // ACT
+            weather.GetWeatherForecast()
+                   .ShouldBe(EqualToPattern("/Patterns/GetWeatherForecast.json").Ignore("$.response.content"))
+                   .ShouldBe(InStatus(HttpStatusCode.OK));
+
+            // TODO: Dodaj inne Should'y
+        }
+
+        private FullOperationPattern EqualToPattern([PathReference] string file)
+            => new FullOperationPattern(Path + file);
+
+        private ResponseStatusPattern InStatus(HttpStatusCode status)
+            => new ResponseStatusPattern(status);
+    }
+}
