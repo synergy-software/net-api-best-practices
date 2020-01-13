e7a626a9ce0e24d70142b53706e92da6d4c48d9a
Marcin Celej <marcin.celej@gmail.com>
2020-01-13 09:38:10 +0100

    I added simple User domain - working in memory

---
+++ Samples/Sample.Web.API/Web.API.Services/Users/Domain/User.cs
@@ -0,0 +1,14 @@
+﻿namespace Synergy.Samples.Web.API.Services.Users.Domain
+{
+    public class User
+    {
+        public string Id { get; }
+        public string Login { get; }
+
+        public User(string id, string login)
+        {
+            Id = id;
+            Login = login;
+        }
+    }
+}
---
+++ Samples/Sample.Web.API/Web.API.Services/Users/Domain/UserRepository.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Synergy.Contracts;
+using Synergy.Samples.Web.API.Services.Infrastructure.Annotations;
+
+namespace Synergy.Samples.Web.API.Services.Users.Domain
+{
+    [CreatedImplicitly]
+    public class UserRepository:IUserRepository
+    {
+        private readonly List<User> users = new List<User>();
+
+        public Task<User> CreateUser(string login)
+        {
+            var user = new User(Guid.NewGuid().ToString().Replace("-", ""), login);
+            this.users.Add(user);
+            return Task.FromResult(user);
+        }
+
+        public Task<User?> FindUserBy(string userId)
+        {
+            User? user = users.FirstOrDefault(u => u.Id == userId);
+            return Task.FromResult(user);
+        }
+
+        private async Task<User> GetUserBy(string userId)
+        {
+            var user = await FindUserBy(userId);
+            Fail.IfNull(user, Violation.Of("There is no user with id '{0}'", userId));
+            return user;
+        }
+
+        public async Task DeleteUser(string userId)
+        {
+            var user = await GetUserBy(userId);
+            users.Remove(user);
+        }
+    }
+
+    public interface IUserRepository
+    {
+        Task<User> CreateUser(string login);
+        Task<User?> FindUserBy(string userId);
+        Task DeleteUser(string userId);
+    }
+}
